9f998c1942aba28b7ad9e7534ef152a1fce05e6c
songtomtom <tom.song@luxrobo.com>
2023-04-12 15:41:59 +0900

    파일 다운 아이디 추출 완료

--- main.go
+++ main.go
@@ -2,27 +2,43 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"github.com/PuerkitoBio/goquery"
+	"log"
 	"net/http"
+	"regexp"
 )
 
 func main() {
-	url := "https://www.suneung.re.kr/boardCnts/fileDown.do?fileSeq=f7683448be641c3b3f47900702ea29f2"
-	response, err := http.Get(url)
+	res, err := http.Get("https://www.suneung.re.kr/boardCnts/list.do?type=default&page=1&searchStr=&m=0403&C06=&boardID=1500234&C05=&C04=&C03=&C02=&searchType=S&C01=&s=suneung")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	defer response.Body.Close()
+	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
-	err = ioutil.WriteFile("file.pdf", body, 0644)
+	// HTML 문서를 파싱합니다.
+	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	fmt.Println("파일 다운로드 완료")
+	// 원하는 DOM 요소를 찾고 조작합니다.
+	doc.Find("a[onclick^=\"fn_fileDown\"]").Each(
+		func(i int, s *goquery.Selection) {
+			// 선택된 각 요소에서 onclick 속성 값을 가져옵니다.
+			res, exists := s.Attr("onclick")
+			if exists {
+				// 정규식을 사용하여 필요한 문자열만 추출합니다.
+				re := regexp.MustCompile(`'([\w\d]+)'`)
+				matches := re.FindStringSubmatch(res)
+
+				if len(matches) > 1 {
+					fmt.Printf("Found result %d: %s\n", i, matches[1])
+				}
+			}
+		},
+	)
 }
