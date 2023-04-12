e0f6696fa6232fac3ba26ed4746666b93c1d7c1a
songtomtom <tom.song@luxrobo.com>
2023-04-12 16:09:20 +0900

    ㅇㅇ

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"regexp"
 )
 
-func main() {
-	res, err := http.Get("https://www.suneung.re.kr/boardCnts/list.do?type=default&page=1&searchStr=&m=0403&C06=&boardID=1500234&C05=&C04=&C03=&C02=&searchType=S&C01=&s=suneung")
+func fetchHashesFromPage(page int) []string {
+	hashes := []string{}
+
+	url := fmt.Sprintf(
+		"https://www.suneung.re.kr/boardCnts/list.do?type=default&m=0403&boardID=1500234&searchType=S&C02=%EC%98%81%EC%96%B4&s=suneung&page=%d",
+		page,
+	)
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,26 +28,65 @@ func main() {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
-	// HTML 문서를 파싱합니다.
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 원하는 DOM 요소를 찾고 조작합니다.
 	doc.Find("a[onclick^=\"fn_fileDown\"]").Each(
 		func(i int, s *goquery.Selection) {
-			// 선택된 각 요소에서 onclick 속성 값을 가져옵니다.
 			res, exists := s.Attr("onclick")
 			if exists {
-				// 정규식을 사용하여 필요한 문자열만 추출합니다.
 				re := regexp.MustCompile(`'([\w\d]+)'`)
 				matches := re.FindStringSubmatch(res)
 
 				if len(matches) > 1 {
-					fmt.Printf("Found result %d: %s\n", i, matches[1])
+					hashes = append(hashes, matches[1])
 				}
 			}
 		},
 	)
+
+	return hashes
+}
+
+func downloadFile(hash string) {
+	url := fmt.Sprintf("https://www.suneung.re.kr/boardCnts/fileDown.do?fileSeq=%s", hash)
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// 'files' 디렉토리가 없으면 생성합니다.
+	err = os.MkdirAll("files", 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	// 파일 경로를 'files' 디렉토리로 변경합니다.
+	filePath := filepath.Join("files", fmt.Sprintf("%s.zip", hash))
+	err = ioutil.WriteFile(filePath, body, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("File %s downloaded\n", filePath)
+}
+
+func main() {
+	startPage := 1
+	endPage := 5
+
+	for page := startPage; page <= endPage; page++ {
+		hashes := fetchHashesFromPage(page)
+		for _, hash := range hashes {
+			downloadFile(hash)
+		}
+	}
 }
