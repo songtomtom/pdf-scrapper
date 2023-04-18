483eb5478b6d1c44595c189cd051edec4615dd4e
songtomtom <tom.song@luxrobo.com>
2023-04-18 17:46:23 +0900

    웹 스크래퍼 정리 되가는중

--- main.go
+++ main.go
@@ -8,18 +8,25 @@ import (
 )
 
 const (
-	homepageUrl = "https://www.suneung.re.kr/boardCnts/list.do?type=default&page=1&searchStr=&m=0403&C06=&boardID=1500234&C05=&C04=&C03=&C02=&searchType=S&C01=&s=suneung"
+	baseURL = "https://www.suneung.re.kr/boardCnts/list.do?type=default&m=0403&boardID=1500234&searchType=S&C02=%EC%98%81%EC%96%B4&s=suneung&page=%d"
 )
 
 func main() {
 
-	htmlString, err := pdfscrapper.FetchHTML(homepageUrl)
-	if err != nil {
-		panic(err)
-	}
+	startPage := 1
+	endPage := 5 // 원하는 마지막 페이지 번호를 설정하세요.
 
-	err = pdfscrapper.FindDivCntBody(htmlString)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+	for page := startPage; page <= endPage; page++ {
+		url := fmt.Sprintf(baseURL, page)
+		htmlString, err := pdfscrapper.FetchHTML(url)
+		if err != nil {
+			fmt.Printf("Error fetching page %d: %v\n", page, err)
+			continue
+		}
+
+		err = pdfscrapper.FindDivCntBody(htmlString)
+		if err != nil {
+			fmt.Printf("Error processing page %d: %v\n", page, err)
+		}
 	}
 }
--- pdfscrapper/pdfscrapper.go
+++ pdfscrapper/pdfscrapper.go
@@ -9,6 +9,16 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type TableRow struct {
+	Id        string
+	Year      string
+	Subject   string
+	Contents  string
+	Date      string
+	ViewCount string
+	Files     []string
+}
+
 func FetchHTML(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -30,13 +40,39 @@ func FindDivCntBody(htmlString string) error {
 		return err
 	}
 
-	doc.Find("div.cntBody.clearfix").Each(
+	var rows []TableRow
+
+	doc.Find("div.cntBody.clearfix tr").Each(
 		func(i int, s *goquery.Selection) {
+			row := TableRow{}
+
 			// 여기에서 필요한 정보를 추출하고 처리합니다.
 			// 예를 들어, 다음 코드는 선택한 태그의 모든 텍스트를 출력합니다.
-			fmt.Println(s.Text())
+
+			if s.Find("td").Eq(2).Text() != "영어" {
+				return
+			}
+
+			row.Id = s.Find("td").Eq(0).Text()
+			row.Year = s.Find("td").Eq(1).Text()
+			row.Subject = s.Find("td").Eq(2).Text()
+			row.Contents = s.Find("td .link a").Text()
+			row.Date = s.Find("td").Eq(4).Text()
+			row.ViewCount = s.Find("td").Eq(5).Text()
+
+			s.Find("td a").Each(
+				func(i int, a *goquery.Selection) {
+					title, _ := a.Attr("title")
+					if title != "" {
+						row.Files = append(row.Files, title)
+					}
+				},
+			)
+			rows = append(rows, row)
 		},
 	)
+
+	fmt.Print(rows)
 
 	return nil
 }
